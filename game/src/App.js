1964fd0121b775c645334a286e58d09d623c7f34
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-16 17:24:45 -0600

    Added the useState hook to React, added a loop to populate the grid, hardcoded the rows and columns, and added comments to each line.

--- game/src/App.js
+++ game/src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
+const numRows = 25;
+const numCols = 25;
 
 const App = () => {
+  const [grid, setGrid] = useState(() => {
+    // Function only runs once state is initialized
+    const rows = []; // Create rows
+    for (let i = 0; i < numRows; i++) {
+      // Use Array.from to create an array filled with 0's. Similar to [None] * Elements in Python
+      rows.push(Array.from(Array(numCols), () => 0)); //and create columns. Second parameter of Array.from is a mapping function that gets the value and the key and you can also return what the value is going to be
+    }
+  });
   return <div>Earth to Bob</div>;
 };
 
