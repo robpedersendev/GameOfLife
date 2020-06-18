42269cc677dcb8d0c6450c451c9a6f9d384430d2
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-17 21:28:08 -0600

    I started all the way over on a new branch. None of the MVP reqs were met

---
+++ build/precache-manifest.beb899daa574a9b43103c09dd1efd903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276591ee4b4716dbdcd995cba99c9afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d7304f9b9c5b33ac8b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9672d4a55010aca476bf",
+    "url": "/static/js/2.f93330f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f93330f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d7304f9b9c5b33ac8b",
+    "url": "/static/js/main.2884dbe2.chunk.js"
+  },
+  {
+    "revision": "ea357716c5efd307342d",
+    "url": "/static/js/runtime-main.dcd612e7.js"
+  }
+]);
