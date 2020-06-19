972d2b0d709f27f53506fab60cba63e5f0dd97d9
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-18 22:54:08 -0600

    added comments

---
+++ src/doublebuffer.js
@@ -0,0 +1,22 @@
+/*
+
+This is a class function that copies the original state into a copied state object and then manipulates the copied state object in a specific way and then returns/replaces that copied state object with the original state. This process is repeated over and over.
+
+Create class to hols all functions
+
+create a constructor with empty state
+
+Create a function to import state
+
+
+create a function that will duplicate state and manipulate it
+    Create function will then apply the game of life rules
+
+
+Create a write/draw function to return copied state to be the new state
+
+Create a function to clear out state
+
+
+
+*/
