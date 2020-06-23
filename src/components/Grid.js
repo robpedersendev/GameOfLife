feecdf95094ae6b40e20bc19b04b6dc46ddde2bd
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-22 22:54:58 -0600

    I think I just need to get my array of boxes to display o nthe screen correctly and then I am done.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,39 +2,38 @@ import React, { useState, useEffect } from "react";
 import "../index.css";
 import Square from "./Square.js";
 
-const Grid = (props) => {
-  const [cols, setCols] = useState(props);
-  const [rows, setRows] = useState(props);
-  const [grid, setGrid] = useState(props);
-  const [selectSquare, setSelectSquare] = useState(props);
-  const width = props.cols * 14 + 1;
-  console.log(grid);
-  let rowsArr = [];
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 14 + 1;
+    console.log(this.props.grid);
+    let rowsArr = [];
 
-  let squareClass = "";
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      let boxId = i + "~" + j;
-      // ternary used to select box on (black) or off (on) when just hovering
-      squareClass = grid[i][j] ? "box on" : "box off";
-      rowsArr.push(
-        <Square
-          squareClass={squareClass}
-          key={boxId}
-          boxId={boxId}
-          row={i}
-          col={j}
-          selectSquare={this.props.clickSquare}
-        />
-      );
+    let squareClass = "";
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+        let boxId = i + "~" + j;
+        // ternary used to select box on (black) or off (on) when just hovering
+        squareClass = this.props.grid[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Square
+            squareClass={squareClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectSquare={this.props.clickSquare}
+          />
+        );
+      }
     }
+
+    // boxes pushed into array (rowsArr)
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
   }
-  // boxes pushed into array (rowsArr)
-  return (
-    <div className="grid" style={{ width: width }}>
-      {rowsArr}
-    </div>
-  );
-};
+}
 
 export default Grid;
