cb322307a91d6cc21228bd0dfe5253d7b77a8499
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-23 22:11:44 -0600

    Updated my Grid.js file to use the 'this.' structure

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,39 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 
 import Square from "./Square.js";
 
-const Grid = ({ cols, rows, grid, clickSquare }) => {
-  const width = cols * 14 + 1;
-  console.log(grid);
-  let rowsArr = [];
+class Grid extends Component {
+  render() {
+    const width = this.props.cols * 14 + 1;
+    console.log(this.props.grid);
+    let rows = [];
 
-  let squareClass = "";
-  for (let i = 0; i < rows; i++) {
-    console.log(rows);
-    for (let j = 0; j < cols; j++) {
-      console.log(cols);
-      let boxId = i + "~" + j;
-      // ternary used to select box on (black) or off (on) when just hovering
-      squareClass = grid[i][j] ? "box on" : "box off";
-      rowsArr.push(
-        <Square
-          squareClass={squareClass}
-          key={boxId}
-          boxId={boxId}
-          row={i}
-          col={j}
-          selectSquare={clickSquare}
-        />
-      );
+    let squareClass = "";
+    for (let i = 0; i < this.props.rows; i++) {
+      console.log(this.props.rows);
+      for (let j = 0; j < this.props.cols; j++) {
+        console.log(this.props.cols);
+        let id = i + "~" + j;
+        // ternary used to select box on (black) or off (on) when just hovering
+        squareClass = this.props.grid[i][j] ? "box on" : "box off";
+        rows.push(
+          <Square
+            squareClass={squareClass}
+            key={id}
+            boxId={id}
+            row={i}
+            col={j}
+            selectSquare={this.props.clickSquare}
+          />
+        );
+      }
     }
+
+    // boxes pushed into array (rowsArr)
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rows}
+      </div>
+    );
   }
-
-  // boxes pushed into array (rowsArr)
-  return (
-    <div className="grid" style={{ width: width }}>
-      {rowsArr}
-    </div>
-  );
-};
-
+}
 export default Grid;
